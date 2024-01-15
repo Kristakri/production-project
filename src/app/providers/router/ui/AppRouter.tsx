7f04c816b8039f85e31691bdda5fb7bd3014bba0
Kristakri <nago.gun@mail.ru>
2024-01-15 10:21:34 +0300

    Add Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,14 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => {
-	console.log(routeConfig);
-	console.log(Object.values(routeConfig));
-	console.log(Object.entries(routeConfig));
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
 			<Routes>
 				{Object.values(routeConfig).map(({ element, path }) => (
-					<Route key={path} element={element} path={path} />
+					<Route
+						key={path}
+						path={path}
+						element={<div className='page-wrapper'>{element}</div>}
+					/>
 				))}
 			</Routes>
 		</Suspense>
